928f0a8290ce90082e6ab3d11d21d2f3ba480c82
Abdullah Anaz <abdullahanaz@yahoo.com>
2023-04-09 03:33:23 +0530

    ADD: CartTotal HTML/Style

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -4,7 +4,12 @@ import './CartTotal.css'
 function CartTotal( {items} ) {
   return (
     <div className='CartTotal'>
-
+      <h3>Subtotal(5 items): 
+        <span className='CartTotal-price'>$2,172.38</span>
+      </h3>
+      <button>
+        Proceed to checkout
+      </button>
     </div>
   )
 }
